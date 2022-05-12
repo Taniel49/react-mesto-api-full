c0e81304642088d0f54d40f124b9c1240146699f
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-05-12 06:39:27 +0300

    Проектная работа 13, Бутырский Михаил, Версия 2

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const User = require('../models/user');
 const ValidationError = require('../errors/ValidationError');
 const NotFoundError = require('../errors/NotFoundError');
