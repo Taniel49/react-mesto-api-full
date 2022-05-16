b8ecf8fbbb6cecff4848364e25baddf8339aefeb
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-05-16 17:05:31 +0300

    Проектная работа 14, Бутырский Михаил, Версия 1

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,34 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   createCard, deleteCard, getAllCards, putLike, deleteLike,
 } = require('../controllers/cards');
 
 router.get('/cards', getAllCards);
 
-router.post('/cards', createCard);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }),
+}), createCard);
 
-router.delete('/cards/:cardId', deleteCard);
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
 
-router.put('/cards/:cardId/likes', putLike);
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), putLike);
 
-router.delete('/cards/:cardId/likes', deleteLike);
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteLike);
 
 module.exports = router;
