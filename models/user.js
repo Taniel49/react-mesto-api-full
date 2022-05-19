55b6330fef4dae4d95005125c683fd83e26976a7
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-05-19 13:08:36 +0300

    Проектная работа 14, Бутырский Михаил, Версия 3

--- models/user.js
+++ models/user.js
@@ -45,7 +45,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         throw new AuthError('Неправильная почта или пароль');
