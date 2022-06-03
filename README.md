abd7f8f164fc843902ea9adb636afecee71d753f
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-06-03 07:47:20 +0300

    final 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+51.250.30.85
 
+https://api.firstproject.students.nomoredomains.xyz
+https://firstproject.students.nomoredomains.xyz
--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
