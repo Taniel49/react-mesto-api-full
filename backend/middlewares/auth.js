f56fe5fcf838e419a735467d569d63551b58ac86
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-06-08 16:17:40 +0300

    final 1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(authorization);
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res
