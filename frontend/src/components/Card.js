c42db1f0a02dfbcc3e8c20f9c770d72978a9e3f9
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-06-09 01:19:19 +0300

    final 1

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,7 +3,7 @@ import CurrentUserContext from '../contexts/CurrentUserContext'
 
 function Card(props) {
     const currentUser = React.useContext(CurrentUserContext);
-    const isOwn = props.card.owner._id === currentUser._id;
+    const isOwn = props.card.owner === currentUser._id;
     const cardDeleteButtonClassName = (
         `element__delete-button ${isOwn ? '' : 'element__delete-button_hidden'}`
     );
