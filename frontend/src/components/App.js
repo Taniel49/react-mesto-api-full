22b007417aa918fe25dbbf84cf6fe7949af5b885
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-06-09 00:18:07 +0300

    final 1

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -63,7 +63,7 @@ function App() {
         const isLiked = card.likes.some(i => i._id === currentUser._id);
 
         api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
-            setCards((state) => state.map((c) => c._id === card._id ? newCard : c))
+            setCards((state) => state.map((c) => c._id === card._id ? newCard.data : c))
         }).catch((err) => {
             console.log(err);
         })
@@ -91,7 +91,7 @@ function App() {
 
     function handleUpdateAvatar(item) {
         api.patchAvatar(item).then((res) => {
-            setCurrentUser(res)
+            setCurrentUser(res.data)
         }).then(() => {
             closeAllPopups()
         }).catch((err) => {
