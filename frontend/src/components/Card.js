f8be3392def7c0a843fd117ae541bc3394363713
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-06-09 04:43:58 +0300

    final 1

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -7,7 +7,7 @@ function Card(props) {
     const cardDeleteButtonClassName = (
         `element__delete-button ${isOwn ? '' : 'element__delete-button_hidden'}`
     );
-    const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+    const isLiked = props.card.likes.some(i => i === currentUser._id);
     const cardLikeButtonClassName = (
         `element__like-button ${isLiked ? 'element__like-button_liked' : ''}`
     );
